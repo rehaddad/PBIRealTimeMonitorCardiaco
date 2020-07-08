2daca22321da366eab5a8a8019a95f5209cfd66c
rehaddad <rehaddad@msn.com>
2020-07-08 15:12:22 -0300

    Update Program.cs
    atualizado o código com mais 2 variáveis min e max no JSON

--- Program.cs
+++ Program.cs
@@ -1,17 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
+using System;
 using System.IO;
-using System.Linq;
 using System.Net;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace MonitorCardiacoRealTime
 {
     class Program
     {
-        //private static string realTimePushURL = "https://api.powerbi.com/beta/6ecd73d4-43af-45c5-a552-97627e83c4ab/datasets/ba990c8a-4ef6-40a4-8620-fdbc68096cde/rows?key=%2FveE50MPU%2FgzAdsVxOhz4J65A3IFSuhkUdxEXrHGpKzEAstX%2BioNq4%2BWKDEiD3u1viOqOQns1%2FQdcWDe4%2BjG%2BA%3D%3D";
-        private static string realTimePushURL = "https://api.powerbi.com/beta/8c07f23c-e1b4-48a9-83fe-31b66589cc67/datasets/290ffc9b-f1b9-418b-9722-658d6827f1fe/rows?key=FDE58YVhyLidMLaJfNJsLsuDLz8BHv1SlOQar8kQDKfLcD6tP0ZhFKYP234nJ9pKbP5ToEHO1sUBl5t5gFIPNg%3D%3D";
+        private static string realTimePushURL = "https://api.powerbi.com/beta/5ecd43d4-43af-45c5-a552-97627e83c4ab/datasets/13afbc87-08f9-41e6-a73f-06676d797c79/rows?key=sjBEapK%2FtFKSfFN3wm4QHzMVNANAtc41fm2n736PN39lglALd9SVjjIx7HfgcYv5DlNeclMqg73gEKc5QHCvLA%3D%3D"; 
 
         static void Main(string[] args)
         {
@@ -22,7 +18,9 @@ namespace MonitorCardiacoRealTime
                     // Declare values that we're about to send
                     String currentTime = DateTime.UtcNow.ToString();
                     Random r = new Random();
-                    int currentValue = r.Next(60, 190);
+                    int currentValue = r.Next(50, 200);
+                    int min = r.Next(1, 50);
+                    int max = r.Next(200,250);
 
                     // Send POST request to the push URL
                     // Uses the WebRequest sample code as documented here: https://msdn.microsoft.com/en-us/library/debx8sh9(v=vs.110).aspx
@@ -30,7 +28,9 @@ namespace MonitorCardiacoRealTime
                     request.Method = "POST";
                     // The data should be in JSON format
                     //string postData = $"[{{ 'ts': '{currentTime}', 'value':{currentValue} }}]";
-                    string postData = String.Format("[{{ \"ts\": \"{0}\", \"value\":{1} }}]", currentTime, currentValue);
+                    string postData = String.Format(
+                "[{{ \"ts\": \"{0}\", \"value\":{1}, \"max\":{2}, \"min\":{3} }}]", 
+                currentTime, currentValue, max, min);
                     Console.WriteLine($"Dados enviados via POST: {postData}");
 
                     // Prepare request for sending
